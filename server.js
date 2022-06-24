cd5da79628f2aebee04ff8fbc537ade330817fc5
cobalt88 <vincentteune@gmail.com>
2022-06-24 16:41:16 -0400

    6.24.22
    update server information

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const { MongoClient } = require("mongodb");
 
 
 const PORT = process.env.PORT || 3001;
-const uri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
@@ -17,19 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-
-const dbConnect = async() => {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-dbConnect();
 
 // routes
 app.use(require("./routes/api.js"));
